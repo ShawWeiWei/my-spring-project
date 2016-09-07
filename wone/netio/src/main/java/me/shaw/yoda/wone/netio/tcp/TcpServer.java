f45d1c253e26e242ccb6e75e8a801ff4320a013f
ShawWeiWei <610961191@qq.com>
2016-09-07 23:29:12 +0800

    fix

---
+++ wone/netio/src/main/java/me/shaw/yoda/wone/netio/tcp/TcpServer.java
@@ -0,0 +1,15 @@
+package me.shaw.yoda.wone.netio.tcp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+
+/**
+ * Created by yes on 7/9/16.
+ */
+public class TcpServer {
+    private final ServerBootstrap bootstrap;
+    private final EventLoopGroup bossGroup;
+    private final EventLoopGroup workerGroup;
+
+
+}
